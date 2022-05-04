2b6bdfdeb8409562fa15c6ffde53951a22e38361
Franweb79 <franprietogutweb@gmail.com>
2022-05-04 23:18:50 +0200

    reversed observable works

--- src/app/gifs/gifs-services/gifs/gifs.service.ts
+++ src/app/gifs/gifs-services/gifs/gifs.service.ts
@@ -35,12 +35,48 @@ export class GifsService {
   }
 
   
-  //function to insert values on historic
+  /*
+    function to insert values on historic.
+
+    We will not onlt insert the value, but will make all the logic
+    to show it on the html, also insert it on the local storage
+
+    We want to show the newest search items on the first place,
+    so we will have to reverse the array which contain the historic(_historic)
+
+    We don´t want to modify the original array, it would give lots of problems
+    when reversing several times and such, I think it is better always having a reference
+    to the original array with no modifications
+
+  */
 
   public insertValueHistoric(valueToBeInserted:string){
+
+    //first, we declare a local variable to store the reversed array.
+
+    let reversedArray:string[]=[];
+
+    //we insert new value on original array
+
     this._historic.push(valueToBeInserted);
 
-    this.$historicObserv=of(this._historic);
+    //we push each element of original array to this copy
+
+
+    for (let i of this._historic){
+      reversedArray.push(i);
+    }
+
+    //Then we reverse it
+
+    reversedArray.reverse();
+
+    /*
+      We create an observable of this reversed array, observable will be shown
+      with the async pipe where needed, to avoid using suscribe()
+    */
+
+    this.$historicObserv=of(reversedArray);
 
     this.setItemOnLocalStorage(valueToBeInserted);
 

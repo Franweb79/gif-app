daa3a1f5eef36c03214af6217779ba18b6230fb4
Franweb79 <franprietogutweb@gmail.com>
2022-06-16 22:26:46 +0200

    cookies working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CookiesComponent } from './shared/shared-components/cookies/cookies.component';
+import { PrivacyComponent } from './shared/shared-components/privacy/privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path:'privacy',
+    component:PrivacyComponent
+  },
+  {
+    path:'cookies',
+    component:CookiesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 
 import { NavbarComponent } from './shared-components/navbar/navbar.component';
 import { CookieBarComponent } from './shared-components/cookie-bar/cookie-bar.component';
 import { CookiesComponent } from './shared-components/cookies/cookies.component';
-
+import { PrivacyComponent } from './shared-components/privacy/privacy.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     CookieBarComponent,
-    CookiesComponent
+    CookiesComponent,
+    PrivacyComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
     BrowserAnimationsModule,
     TranslateModule //TODO PON QUE TUVISTE QUE PONERLO AQUI TB EN EL SHARED MODULE PA QUE 
     //TRANSLATE PIPE EN EL HTML DEL COOKIE BAR NO DIERA ERROR
@@ -29,6 +32,7 @@ import { CookiesComponent } from './shared-components/cookies/cookies.component'
     NavbarComponent,
     CookieBarComponent,
     CookiesComponent,
+    PrivacyComponent,
     TranslateModule
 ]
     

d185ed4a5bf73d7bb7ec4b9a552e4ce5f251643e
Franweb79 <franprietogutweb@gmail.com>
2022-06-21 17:16:45 +0200

    giphy attribution marks added

--- src/app/gifs/gifs-services/social-service/social.service.ts
+++ src/app/gifs/gifs-services/social-service/social.service.ts
@@ -14,12 +14,48 @@ export class SocialService {
   share(socialNetWorkName:string, urlString:string){
 
     if(socialNetWorkName==='facebook'){
-      alert("share facebook");
+     // alert("share facebook");
 
       FB.ui({
-        method: 'share',
-        href: urlString
-      });
+
+        method: 'share_open_graph',
+        action_type:'og.shares',
+        action_properties: JSON.stringify({
+          object : {
+             'og:url': urlString, // your url to share,
+             'og_image':urlString
+            
+             
+             
+          }
+
+        })
+        
+       /* method:'share',
+        href:urlString*/
+
+          /* TODO https://stackoverflow.com/questions/10444347/share-link-to-posts-when-using-fb-ui*/
+        })
+
+        /*
+
+        method: 'share_open_graph',
+        action_type:'og.shares',
+        action_properties: JSON.stringify({
+          object : {
+             'og:url': urlString, // your url to share,
+             'og_image':urlString
+            
+             
+             
+          }
+
+        });
+        */
+        /*href: urlString,
+        quote: "Hi there"*/
+      
+
 
     }
   }
